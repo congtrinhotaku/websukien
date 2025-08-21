9f9d237dcf350a1b6cde7266a80dfd7aff0c29b0
kazuto <congtrinhotaku2@gmail.com>
2025-08-21 16:27:37 +0700

    sua lai

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
-var createError = require('http-errors');
-var express = require('express');
-const env = require("dotenv").config();
-const session = require ("express-session");
-const methodOverride = require('method-override');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express")
+const app = express()
+const env = require("dotenv").config()
+const path = require("path")
+const session = require("express-session")
+const methodOverride = require("method-override")
+const db = require("./config/db")
+const morgan = require("morgan")
+// Router
+const indexRouter = require("./routes/index")
+const usersRouter = require("./routes/users")
 
-const db = require ("./config/db")
+// Kết nối DB
+db()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-db()    
-
-app.use(methodOverride('_method'));
-app.use(express.json()) 
-app.use(express.urlencoded({extended:true}))
+// Middleware
+app.use(methodOverride("_method"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(session({
-    secret:process.env.SECRET_KEY, 
-    resave:false,  
-    saveUninitialized:true, 
-    cookie:{
-        secure:false,
-        httpOnly:true,
-        maxAge:72*60*60*1000
-    }
-})) 
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    maxAge: 72 * 60 * 60 * 1000
+  }
+}))
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-app.listen(process.env.PORT,()=>{
-    console.log("Server is running http://localhost:"+process.env.PORT)
+// Ngăn cache
+app.use((req, res, next) => {
+  res.set("cache-control", "no-store")
+  next()
 })
 
-module.exports = app;
+// View engine
+app.set("view engine", "ejs")
+app.set("views", [
+  path.join(__dirname, "views"),
+])
+
+// Static files
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+
+// Routes
+app.use("/", indexRouter)
+app.use("/users", usersRouter)
+
+// Error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const errorMessage = err.message || "Internal Server Error"
+  res.status(statusCode).render("errorPage", { statusCode, errorMessage })
+})
+
+// 404 page
+app.use((req, res) => {
+  res.send("404 Not Found");
+})
+
+// Start server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running http://localhost:${process.env.PORT}`)
+})
+
+module.exports = app
