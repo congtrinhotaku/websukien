ab2cbfbcede4320b6e8202e1314a3a797c9a0630
Phan Thanh Chien <chiendtr0@gmail.com>
2025-08-21 23:15:21 +0700

    hien thi dssk

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 const userController = require("../controllers/userController");
+const trangchuController = require("../controllers/trangchuController");
+
+
+
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('trangchu', { });
-});
 
 
+router.get("/", trangchuController.getHomePage);
 router.get("/register", userController.loadSignup);
 router.post("/register", userController.signup);
 router.get("/login", userController.loadLogin);
 router.post("/login", userController.login);
+
 
 module.exports = router;
